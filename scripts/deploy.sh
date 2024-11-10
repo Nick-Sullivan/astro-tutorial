a04f36ad09b1688e6c867a8c6979f6383c5537a4
Nick Sullivan <nick.dave.sullivan@gmail.com>
2024-11-10 09:30:43 +0000

    trying to fix deploy permissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,6 @@
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 ASTRO_DIR="$SCRIPT_DIR/../astro"
 
+echo "Deploying website"
+
 aws s3 sync $ASTRO_DIR/dist/ s3://nicksastroexperiment.com/ --delete
